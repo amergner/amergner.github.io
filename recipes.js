1cac0244c428b366f87ff85e699f31b22e654398
amergner <arthur.mergner@u.northwestern.edu>
2021-05-09 11:12:14 -0500

    fancier recipe functionality

--- recipes.js
+++ recipes.js
@@ -1,13 +1,46 @@
 var recipes = [
-	"Chili",
+	"Barbecue Chicken Pizza",
+	"Beef Florentine",
+	"Beef Stroganoff",
 	"Chicken & Sausage",
-	"Stir Fry",
-	"Ham, Peas & Pasta",
 	"Chicken & Smashed Potatoes",
+	"Chicken Burritos",
+	"Chicken Cacciatore",
+	"Chicken Marsala",
+	"Chicken Parmesan",
+	"Chicken Pot Pie",	
+	"Chili",
+	"Crab & Linguine",
+	"Crumbly Chicken",
+	"Deep Dish Pizza",
+	"Fajitas",
 	"Falafel",
+	"Ham, Peas & Pasta",
+	"Hamburgers",
+	"Lasagna",
+	"Lentil Daal",
+	"Meatball Subs",
+	"Meatloaf",
+	"Pasta Vodka Sauce",
+	"Pasties",
+	"Pepper Steak",
+	"Porcupines",
+	"Pork Cutlets",
 	"Sloppy Joes",
-	"Meatloaf"
+	"Spaghetti",
+	"Stir Fry",
+	"Stuffed Shells",
+	"Sweet Potato Chili",
+	"Tacos",
+	"Teriyaki Steak",
+	"Tilapia",
+	"Tofu, Veggies and Sauce",
+	"Tuna Salad",
+	"Turkey Burgers",
+	"White Lasagna"
 ];
+
+// Random Recipe Generator
 
 var numRecipes = recipes.length;
 
@@ -15,4 +48,20 @@ function selectRecipe() {
 	var randNum = Math.floor(Math.random() * numRecipes);
 	console.log(randNum);
 	document.getElementById("selectedRecipe").innerHTML = recipes[randNum];
+}
+
+// Typing Effect Function
+var i = 0;
+var txt = 'The RAT KING cordially invites you to the SEWER for a TASTEFUL DINING EXPERIENCE.';
+var txt2 = 'How do you ACCEPT this invitation?';
+var txt3 = '> With GRATITUDE';
+var txt4 = '> With ENTHUSIASM';
+var speed = 50;
+
+function typeWriter() {
+	if (i < txt.length) {
+		document.getElementById("demo").innerHTML += txt.charAt(i);
+		i++;
+		setTimeout(typeWriter, speed);
+	}
 }
